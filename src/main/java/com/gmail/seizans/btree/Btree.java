ad2d71ac947670ca53dcbbdffd191d23d4b98fae
seizans <seizans@gmail.com>
2011-11-14 09:04:18 +0900

    fixed insert and search method.

--- src/main/java/com/gmail/seizans/btree/Btree.java
+++ src/main/java/com/gmail/seizans/btree/Btree.java
@@ -1,9 +1,13 @@
 package com.gmail.seizans.btree;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
 
-public final class Btree<E extends Comparable<E>> {
+public final class Btree<E extends Comparable<E>> implements Iterable<E> {
 	private final int degree;
 	private final List<E> keys;
 	private final List<Btree<E>> children;
@@ -39,42 +43,93 @@ public final class Btree<E extends Comparable<E>> {
 		if (! child.isFull()) {
 			return;
 		}
+		
+		// TODO 以下の検証時用のバグ判別if文を後で除去する
+		if (child.keys.size() != degree * 2 - 1) {
+			System.out.println(child.keys.size());
+			System.out.println(child.keys);
+			throw new IllegalStateException();
+		}
 
 		// まんなかのノードを持ち上げる
 		keys.add(idx, child.keys.get(degree - 1));
 
 		// 右と中の間に右半分への参照を代入する
-		Btree<E> newTree = new Btree<E>(degree, child.isLeaf);
-		for (int i = degree; i < child.keys.size(); i++) {
-			newTree.keys.add(child.keys.get(i));
+		Btree<E> newLhs = new Btree<E>(degree, child.isLeaf);
+		if (child.isLeaf) {
+			for (int i = 0; i < degree - 1; i++) {
+				newLhs.keys.add(child.keys.get(i));
+			}
+		} else {
+			for (int i = 0; i < degree - 1; i++) {
+				newLhs.keys.add(child.keys.get(i));
+				newLhs.children.add(child.children.get(i));
+			}
+			newLhs.children.add(child.children.get(degree - 1));
 		}
-		children.add(idx + 1, newTree);
+		
+		Btree<E> newRhs = new Btree<E>(degree, child.isLeaf);
+//		for (int i = degree; i < child.keys.size(); i++) {
+		if (child.isLeaf) {
+			for (int i = degree; i < degree * 2 - 1; i++) {
+				newRhs.keys.add(child.keys.get(i));
+			}
+		} else {
+			for (int i = degree; i < degree * 2 - 1; i++) {
+				newRhs.keys.add(child.keys.get(i));
+				newRhs.children.add(child.children.get(i));
+			}
+			newRhs.children.add(child.children.get(degree * 2 - 1));
+		}
+		child.keys.remove(degree - 1);
+
+		children.remove(idx);
+		children.add(idx, newRhs);
+		children.add(idx, newLhs);
 
 		// 左と中の間に左半分への参照を代入する
-		for (int i = degree * 2 - 1; i <= degree - 1; i--) {
-			child.keys.remove(i);
+//		for (int i = degree * 2 - 2; i >= degree; i--) {
+//			child.keys.remove(i);
+//		}
+
+		// TODO 以下の検証時用のバグ判別if文を後で除去する
+		if (children.get(idx).keys.size() != degree - 1) {
+			System.out.println(children.get(idx).keys.size());
+			System.out.println(children.get(idx + 1).keys.size());
+			System.out.println(children.get(idx).keys);
+			System.out.println(children.get(idx + 1).keys);
+			throw new IllegalStateException();
 		}
 	}
 
 	private boolean isFull() {
-		return keys.size() == degree * 2 - 1;
+		return keys.size() >= degree * 2 - 1;
 	}
 
 	public boolean search(E e) {
-		for (int i = 0; i < keys.size(); i++) {
-			E key = keys.get(i);
-			if (key.compareTo(e) == 0) {
-				return true;
+		if (isLeaf) {
+			for (int i = 0; i < keys.size(); i++) {
+				E key = keys.get(i);
+				if (e.equals(key)) {
+					return true;
+				}
 			}
-			if (key.compareTo(e) < 0) {
-				return children.get(i).search(e);
+			return false;
+		} else {
+			for (int i = 0; i < keys.size(); i++) {
+				E key = keys.get(i);
+				if (e.equals(key)) {
+					return true;
+				}
+				if (e.compareTo(key) < 0) {
+					return children.get(i).search(e);
+				}
 			}
+			return children.get(keys.size()).search(e);
 		}
-		return false;
 	}
 
 	public void insert(E e) {
-		System.out.println(e);
 		if (isRoot) {
 			if (isFull()) {
 				rootSplit();
@@ -83,6 +138,10 @@ public final class Btree<E extends Comparable<E>> {
 		if (isLeaf) {
 			leafInsert(e);
 		} else {
+			// TODO 削除する 検証用コード
+			if (children.size() == 0) {
+				System.out.println("fuga");
+			}
 			nodeInsert(e);
 		}
 	}
@@ -94,6 +153,7 @@ public final class Btree<E extends Comparable<E>> {
 			}
 			if (e.compareTo(keys.get(i)) < 0) {
 				keys.add(i, e);
+				return;
 			}
 		}
 		keys.add(e);
@@ -118,7 +178,27 @@ public final class Btree<E extends Comparable<E>> {
 				}
 			}
 		}
-		children.get(children.size() - 1).insert(e);
+		if ( children.size() == 0) {
+			System.out.println(e);
+			System.out.println(keys);
+			System.out.println(children);
+			System.out.println("hoge");
+		}
+		
+		int size = children.size();
+		splitChild(children.size() - 1);
+		if (size != children.size()) {
+			if (e.equals(keys.get(size - 1))) {
+				throw new IllegalArgumentException("Duplication element");
+			}
+			if (e.compareTo(keys.get(size - 1)) < 0) {
+				children.get(size - 1).insert(e);
+			} else {
+				children.get(size).insert(e);
+			}
+		} else {
+			children.get(children.size() - 1).insert(e);
+		}
 	}
 
 	private void rootSplit() {
@@ -133,7 +213,7 @@ public final class Btree<E extends Comparable<E>> {
 		Btree<E> lhs = new Btree<E>(degree, isLeaf);
 		Btree<E> rhs = new Btree<E>(degree, isLeaf);
 		for (int i = 0; i < degree - 1; i++) {
-			lhs.keys.add(0, keys.get(i));
+			lhs.keys.add(keys.get(i));
 		}
 		for (int i = degree; i < degree * 2 - 1; i++) {
 			rhs.keys.add(keys.get(i));
@@ -154,10 +234,12 @@ public final class Btree<E extends Comparable<E>> {
 			lhs.keys.add(keys.get(i));
 			lhs.children.add(children.get(i));
 		}
+		lhs.children.add(children.get(degree - 1));
 		for (int i = degree; i < degree * 2 - 1; i++) {
 			rhs.keys.add(keys.get(i));
 			rhs.children.add(children.get(i));
 		}
+		rhs.children.add(children.get(degree * 2 - 1));
 		E temp = keys.get(degree - 1);
 		keys.clear();
 		keys.add(temp);
@@ -168,5 +250,95 @@ public final class Btree<E extends Comparable<E>> {
 
 	public void delete(E e) {
 		
+	}
+
+//	@Override
+//	public String toString() {
+//		return 
+//	}
+
+	public Iterator<E> iterator() {
+		return new OrderIterator();
+	}
+
+	private class OrderIterator implements Iterator<E> {
+		private final Stack<Object> stack = new Stack<Object>();
+
+		private OrderIterator() {
+			Btree<E> root = Btree.this;
+			if (root.isLeaf) {
+				if (root.keys.size() == 0) {
+					return;
+				}
+				leafPush(root);
+			} else {
+				nodePush(root);
+			}
+		}
+
+		private void leafPush(Btree<E> btree) {
+			ListIterator<E> iter = btree.keys.listIterator(btree.keys.size());
+			while (iter.hasPrevious()) {
+				stack.push(iter.previous());
+			}
+		}
+
+		private void nodePush(Btree<E> btree) {
+			for (int i = btree.keys.size() - 1; i >= 0; i--) {
+				stack.push(btree.children.get(i + 1));
+				stack.push(btree.keys.get(i));
+			}
+			stack.push(btree.children.get(0));
+		}
+
+		public boolean hasNext() {
+			return ! stack.empty();
+		}
+
+		public E next() {
+			if (! hasNext()) {
+				throw new NoSuchElementException();
+			}
+			Object obj = stack.pop();
+			if (obj instanceof Comparable<?>) {
+				E key = (E)obj;
+				return key;
+			} else if (obj instanceof Btree<?>) {
+				Btree<E> child = (Btree<E>) obj;
+				if (child.isLeaf) {
+					leafPush(child);
+				} else {
+					nodePush(child);
+				}
+				return next();
+			} else {
+				throw new IllegalStateException();
+			}
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		if (keys.isEmpty()) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		if (!isLeaf) {
+			for (int i = 0; i < keys.size(); i++) {
+				sb.append("[" + children.get(i) + "]");
+				sb.append("(" + keys.get(i) + ")");
+			}
+			sb.append("[" + children.get(children.size() - 1) + "]");
+		} else {
+			for (int i = 0; i < keys.size(); i++) {
+				sb.append("(" + keys.get(i) + ")");
+			}
+		}
+		return sb.toString();
 	}
 }
--- src/test/java/com/gmail/seizans/btree/BtreeTest.java
+++ src/test/java/com/gmail/seizans/btree/BtreeTest.java
@@ -1,18 +1,90 @@
 package com.gmail.seizans.btree;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
 public class BtreeTest {
+	
+	@Test
+	public void insertTest3() {
+		Btree<Integer> btree = new Btree<Integer>(2);
+		btree.insert(4);
+		btree.insert(2);
+		btree.insert(10);
+		btree.insert(8);
+		btree.insert(3);
+		btree.insert(9);
+		btree.insert(1);
+		btree.insert(5);
+	}
+
+	@Test
+	public void insertTest2() {
+		Btree<Integer> btree = new Btree<Integer>(2);
+		for (int i = 100; i > 50; i--) {
+			Integer in = Integer.valueOf(i);
+			btree.insert(in);
+		}
+		System.out.println(btree);
+		for (Integer i : btree) {
+			System.out.println(i);
+		}
+	}
+
+	@Test
+	public void zeroTest() {
+		Btree<Integer> btree = new Btree<Integer>(4);
+		for (Integer i: btree) {
+			System.out.println(i);
+		}
+		btree.insert(0);
+		btree.insert(1);
+		btree.insert(2);
+		btree.insert(3);
+		btree.insert(4);
+		btree.insert(5);
+		btree.insert(6);
+		btree.insert(7);
+		btree.insert(8);
+		for (Integer i: btree) {
+			System.out.println(i);
+		}
+	}
 
 	@Test
 	public void insertTest() {
 		final int N = 50;
-		Btree<Integer> btree = new Btree<Integer>(3);
+		Btree<Integer> btree = new Btree<Integer>(2);
 		for (int i = 0; i < N; i++) {
 			btree.insert(i);
 		}
+
+		for (Integer i: btree) {
+			if (!btree.search(i)) {
+				System.out.println(i);
+				System.out.println(btree);
+			}
+			assertThat(btree.search(i), is(true));
+		}
+
+		Iterator<Integer> iter = btree.iterator();
+		for (int i = 0; i < N; i++) {
+			assertThat(iter.hasNext(), is(true));
+			assertThat(iter.next(), is(i));
+		}
+		assertThat(iter.hasNext(), is(false));
+		try {
+			iter.next();
+			fail();
+		} catch (NoSuchElementException e) {
+		}
+
 	}
 
 	@Test
